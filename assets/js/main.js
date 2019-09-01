1eb2ebeac31410e22cf04589fc8a97825282f13c
EllisHughes <ellis.hughes88@outlook.com>
2019-09-01 15:41:55 +0100

    price is cleared on backspace

--- assets/js/main.js
+++ assets/js/main.js
@@ -3505,7 +3505,6 @@ var companies = [
     'Zynerba Pharmaceuticals, Inc. ZYNE',
     'Zynex, Inc. ZYXI',
     'Zynga Inc. ZNGA',
-    
 
 
 
@@ -3520,7 +3519,8 @@ var companies = [
 
 
 
-    
+
+
 ];
 
 // var json = (function() {
@@ -3554,13 +3554,13 @@ var doneTypingInterval = 1; //time in ms, 5 second for example
 var $input = $("#myInput");
 
 //on keyup, start the countdown
-$input.on("keyup", function() {
+$input.on("keyup", function () {
     clearTimeout(typingTimer);
     typingTimer = setTimeout(doneTyping, doneTypingInterval);
 });
 
 //on keydown, clear the countdown
-$input.on("keydown", function() {
+$input.on("keydown", function () {
     clearTimeout(typingTimer);
 });
 
@@ -3571,20 +3571,20 @@ function doneTyping() {
     // fetch(
     //     `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${search}&apikey=1YK0TA3NL2XSK2QF`
     // )
-        // .then(function(response) {
-        //     return response.json();
-        // })
-        // .then(function(myJson) {
-        //     companies = [];
-        //     for (let i = 0; i < 5; i++) {
-            //     const match = myJson.bestMatches[i];
-            //     const symbol = match["1. symbol"];
-            //     const name = match["2. name"];
-            //     companies.push(`${name} (${symbol})`);
-            // }
-            // console.log(companies);
-            autocomplete(document.getElementById("myInput"), companies);
-        // });
+    // .then(function(response) {
+    //     return response.json();
+    // })
+    // .then(function(myJson) {
+
+    //     for (let i = 0; i < 5; i++) {
+    //     const match = myJson.bestMatches[i];
+    //     const symbol = match["1. symbol"];
+    //     const name = match["2. name"];
+    //     companies.push(`${name} (${symbol})`);
+    // }
+    // console.log(companies);
+    autocomplete(document.getElementById("myInput"), companies);
+    // });
 }
 
 function autocomplete(inp, arr) {
@@ -3592,7 +3592,7 @@ function autocomplete(inp, arr) {
 	the text field element and an array of possible autocompleted values:*/
     var currentFocus;
     /*execute a function when someone writes in the text field:*/
-    inp.addEventListener("input", function(e) {
+    inp.addEventListener("input", function (e) {
         var a,
             b,
             i,
@@ -3624,37 +3624,37 @@ function autocomplete(inp, arr) {
                 /*insert a input field that will hold the current array item's value:*/
                 b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
                 /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function(e) {
+                b.addEventListener("click", function (e) {
                     /*insert the value for the autocomplete text field:*/
                     inp.value = this.getElementsByTagName("input")[0].value;
                     var tag = this.getElementsByTagName("input")[0].value;
-                    
-                   function test(input) {
-                       var n = input.split(" ");
-                       return n[n.length - 1];
-                   }
+
+                    function test(input) {
+                        var n = input.split(" ");
+                        return n[n.length - 1];
+                    }
                     //tag.match(/\((.*)\)/);
                     var n = tag.split(" ");
                     n = n[n.length - 1];
                     console.log(n)
-                   
+
                     fetch(
                         `https://cloud.iexapis.com/stable/stock/${
-                            n
+                        n
                         }/quote?token=pk_e9e24d928d1f4b4e8b5565d2561c4bb1`
-                    
+
                     )
-                        .then(function(response) {
+                        .then(function (response) {
                             return response.json();
                         })
-                        .then(function(myJson) {
+                        .then(function (myJson) {
                             console.log(myJson.latestPrice);
 
                             document.getElementById(
                                 "price"
                             ).innerHTML = `$${myJson.latestPrice}`;
                         });
-                    
+
                     /*close the list of autocompleted values,
 					(or any other open lists of autocompleted values:*/
                     closeAllLists();
@@ -3664,7 +3664,7 @@ function autocomplete(inp, arr) {
         }
     });
     /*execute a function presses a key on the keyboard:*/
-    inp.addEventListener("keydown", function(e) {
+    inp.addEventListener("keydown", function (e) {
         var x = document.getElementById(this.id + "autocomplete-list");
         if (x) x = x.getElementsByTagName("div");
         if (e.keyCode == 40) {
@@ -3716,7 +3716,17 @@ function autocomplete(inp, arr) {
         }
     }
     /*execute a function when someone clicks in the document:*/
-    document.addEventListener("click", function(e) {
+    document.addEventListener("click", function (e) {
         closeAllLists(e.target);
     });
+
+
+    /* Clears text in price when backspace is pressed*/
+    inp.addEventListener("keydown", function (e) {
+        if (e.keyCode == 8) {
+            document.getElementById(
+                "price"
+            ).innerHTML = ``;
+        }
+    })
 }
