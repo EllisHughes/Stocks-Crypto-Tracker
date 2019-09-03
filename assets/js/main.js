aa3b50ffd9ffb7b0622b3036b58962c26e672da4
EllisHughes <ellis.hughes88@outlook.com>
2019-09-03 01:30:45 +0100

    styling changes / basic layout for crypto.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,6 +146,7 @@ function autocomplete(inp, arr) {
         /*add class "autocomplete-active":*/
         x[currentFocus].classList.add("autocomplete-active");
     }
+    
     function removeActive(x) {
         /*a function to remove the "active" class from all autocomplete items:*/
         for (var i = 0; i < x.length; i++) {
