f6374994d61855b472b084eb9cbd3e32455bcc7d
EllisHughes <ellis.hughes88@outlook.com>
2019-09-05 00:31:26 +0100

    BTC/ETH/LTC functionality

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,5 +1,6 @@
+/* FETCH BITCOIN PRICE */
 function refreshData() {
-    x = 30;
+    x = 10;
     fetch('https://api.coincap.io/v2/assets/bitcoin')
         .then(function (response) {
             return response.json();
@@ -7,11 +8,53 @@ function refreshData() {
         .then(function (myJson) {
             document.getElementById(
                 "cryptoPricesBitcoin"
+            ).innerHTML = `$` + parseFloat(`${myJson.data.priceUsd}`).toLocaleString('en');
+
+
+            document.getElementById(
+                "btc24"
+            ).innerHTML = parseFloat(`${myJson.data.changePercent24Hr}`).toFixed(2) + `%`;
+
+
+
+        });
+
+    fetch('https://api.coincap.io/v2/assets/ethereum')
+        .then(function (response) {
+            return response.json();
+
+        })
+        .then(function (myJson) {
+            document.getElementById(
+                "cryptoPricesEthereum"
             ).innerHTML = `$` + parseFloat(`${myJson.data.priceUsd}`).toFixed(2);
 
-            console.log(myJson.data.changePercent24Hr)
+            document.getElementById(
+                "eth24"
+            ).innerHTML = parseFloat(`${myJson.data.changePercent24Hr}`).toFixed(2) + `%`;
+
+        });
+
+    fetch('https://api.coincap.io/v2/assets/litecoin')
+        .then(function (response) {
+            return response.json();
+
+        })
+        .then(function (myJson) {
+            document.getElementById(
+                "cryptoPricesLitecoin"
+            ).innerHTML = `$` + parseFloat(`${myJson.data.priceUsd}`).toFixed(2);
+
+            document.getElementById(
+                "ltc24"
+            ).innerHTML = parseFloat(`${myJson.data.changePercent24Hr}`).toFixed(2) + `%`;
+
         });
 
     setTimeout(refreshData, x * 1000);
 }
 refreshData();
+
+/* FETCH ETHEREUM PRICE */
+
+
