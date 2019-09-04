98381e524d98ce2466b28f2d276bc52d173d5fd7
EllisHughes <ellis.hughes88@outlook.com>
2019-09-04 22:39:57 +0100

    crypto prices update every 30 seconds

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,11 +1,17 @@
-fetch('https://api.coincap.io/v2/assets/bitcoin')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (myJson) {
-        document.getElementById(
-            "cryptoPricesBitcoin"
-        ).innerHTML = `$` + parseFloat(`${myJson.data.priceUsd}`).toFixed( 2 );
+function refreshData() {
+    x = 30;
+    fetch('https://api.coincap.io/v2/assets/bitcoin')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            document.getElementById(
+                "cryptoPricesBitcoin"
+            ).innerHTML = `$` + parseFloat(`${myJson.data.priceUsd}`).toFixed(2);
 
-        console.log(myJson.data.changePercent24Hr)
-    });
+            console.log(myJson.data.changePercent24Hr)
+        });
+
+    setTimeout(refreshData, x * 1000);
+}
+refreshData();
